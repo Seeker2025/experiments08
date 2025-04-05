cabe0a11b48b35cd45fe0227a48d4d1a65e335d2
Seeker2025 <85400723+Seeker2025@users.noreply.github.com>
2025-04-05 09:48:12 +0000

    Deploying to gh-pages from @ Seeker2025/experiments08@63665cb6a5ccef23c43f4ee1241b77441799b7e8 🚀

---
+++ page2.js
@@ -0,0 +1,2 @@
+import"./assets/modulepreload-polyfill-B5Qt9EMX.js";const t=1e3,e=3;let o=0;const n=setInterval(()=>{if(console.log("Подпишись на рассылку! - "+Date.now()),o+=1,o===e){console.log("Нужно остановить интервал"),clearInterval(n);return}},t);
+//# sourceMappingURL=page2.js.map
