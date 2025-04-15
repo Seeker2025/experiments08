84aa1fdc2ecc31e0a60d77e95bbd19473521b90c
Igor Yurovsky <85400723+Seeker2025@users.noreply.github.com>
2025-04-15 19:06:09 +0300

    go on

--- src/js/page_9.js
+++ src/js/page_9.js
@@ -4,39 +4,89 @@ const start = document.querySelector('.js-start');
 const container = document.querySelector('.js-container');
 
 start.addEventListener('click', onStart);
+// console.log(container.children);
 
-console.log(container.children);
+// function onStart() {
+//     const result = [];
+//     // console.dir(container);
+//     [...container.children].forEach((box ) => {box.textContent = ""});
+//     [...container.children].forEach((box, i) => {
+//         creationPromise(i)
+//         .then((smile)=>{
+//             box.textContent = smile;
+//             result.push('1');
+//         })
+//         .catch((smile)=>box.textContent=smile)
+//         .finally(()=>{
+//             setTimeout(()=>{
+//                 if(i === container.children.length - 1){
+//                     if(!result.length || result.length === 3){
+//                         alert('Winner!');
+//                     }
+//                     else{
+//                         alert('Lost money');
+//                     }
+//                 }
+//             }, 500)
+//          })
+// });
+// }
+
+// function creationPromise(deley) {
+//     return new Promise((res, rej) => {
+//         setTimeout(() => {
+//             const random = Math.random();
+//             if(random > .7){
+//                 res("🤑");
+//             }
+//             else{
+//                 rej("😈");
+//             }
+//         }, 1000 * deley);
+     
+//     })
+// }
+
 function onStart() {
+    let counter = 0;
     const result = [];
     // console.dir(container);
     [...container.children].forEach((box ) => {box.textContent = ""});
-    [...container.children].forEach((box, i) => {
-        creationPromise(i)
-        .then((smile)=>{
-            box.textContent = smile;
-            result.push('1');
-        })
-        .catch((smile)=>box.textContent=smile)
-        .finally(()=>{
+    const promises = [...container.children].map((_, i) => creationPromise(i));
+    // console.log(promises);
+    Promise.allSettled(promises)
+    .then(items =>{
+        items.forEach((item, i)=>{
+            console.log(item);
             setTimeout(()=>{
-                if(i === container.children.length - 1){
-                    if(!result.length || result.length === 3){
-                        alert('Winner!');
-                    }
-                    else{
-                        alert('Lost money');
-                    }
+                if(item.status === "fulfilled"){
+                    counter += 1;
                 }
-            }, 500)
-         })
+                container.children[i].textContent=item.value || item.reason;
+                   
+                if(container.children.length - 1 === i){
+                    setTimeout(()=>{
+                        if(counter === container.children.length || !counter){
+                            alert("Winner!");
+                        }
+                        else{
+                            alert("Lost money!");
+                        }
+                    }, 500);
+                  }
+            }, i * 1000)
+            // console.log(items);
+        })
+
 });
 }
+
 
 function creationPromise(deley) {
     return new Promise((res, rej) => {
         setTimeout(() => {
             const random = Math.random();
-            if(random > .7){
+            if(random > .5){
                 res("🤑");
             }
             else{
