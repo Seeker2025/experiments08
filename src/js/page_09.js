6e25f97dddb18ac5058ca94c2a3c7c45152d2c4f
Igor Yurovsky <85400723+Seeker2025@users.noreply.github.com>
2025-04-20 09:23:29 +0300

    go on

--- src/js/page_09.js
+++ src/js/page_09.js
@@ -6,87 +6,37 @@ const container = document.querySelector('.js-container');
 start.addEventListener('click', onStart);
 // console.log(container.children);
 
-// function onStart() {
-//     const result = [];
-//     // console.dir(container);
-//     [...container.children].forEach((box ) => {box.textContent = ""});
-//     [...container.children].forEach((box, i) => {
-//         creationPromise(i)
-//         .then((smile)=>{
-//             box.textContent = smile;
-//             result.push('1');
-//         })
-//         .catch((smile)=>box.textContent=smile)
-//         .finally(()=>{
-//             setTimeout(()=>{
-//                 if(i === container.children.length - 1){
-//                     if(!result.length || result.length === 3){
-//                         alert('Winner!');
-//                     }
-//                     else{
-//                         alert('Lost money');
-//                     }
-//                 }
-//             }, 500)
-//          })
-// });
-// }
-
-// function creationPromise(deley) {
-//     return new Promise((res, rej) => {
-//         setTimeout(() => {
-//             const random = Math.random();
-//             if(random > .7){
-//                 res("🤑");
-//             }
-//             else{
-//                 rej("😈");
-//             }
-//         }, 1000 * deley);
-     
-//     })
-// }
-
 function onStart() {
-    let counter = 0;
     const result = [];
     // console.dir(container);
     [...container.children].forEach((box ) => {box.textContent = ""});
-    const promises = [...container.children].map((_, i) => creationPromise(i));
-    // console.log(promises);
-    Promise.allSettled(promises)
-    .then(items =>{
-        items.forEach((item, i)=>{
-            console.log(item);
-            setTimeout(()=>{
-                if(item.status === "fulfilled"){
-                    counter += 1;
-                }
-                container.children[i].textContent=item.value || item.reason;
-                   
-                if(container.children.length - 1 === i){
-                    setTimeout(()=>{
-                        if(counter === container.children.length || !counter){
-                            alert("Winner!");
-                        }
-                        else{
-                            alert("Lost money!");
-                        }
-                    }, 500);
-                  }
-            }, i * 1000)
-            // console.log(items);
+    [...container.children].forEach((box, i) => {
+        creationPromise(i)
+        .then((smile)=>{
+            box.textContent = smile;
+            result.push('1');
         })
-
+        .catch((smile)=>box.textContent=smile)
+        .finally(()=>{
+            setTimeout(()=>{
+                if(i === container.children.length - 1){
+                    if(!result.length || result.length === 3){
+                        alert('Winner!');
+                    }
+                    else{
+                        alert('Lost money');
+                    }
+                }
+            }, 500)
+         })
 });
 }
-
 
 function creationPromise(deley) {
     return new Promise((res, rej) => {
         setTimeout(() => {
             const random = Math.random();
-            if(random > .5){
+            if(random > .7){
                 res("🤑");
             }
             else{
@@ -96,3 +46,56 @@ function creationPromise(deley) {
      
     })
 }
+
+
+
+
+// function onStart() {
+//     let counter = 0;
+//     const result = [];
+//     // console.dir(container);
+//     [...container.children].forEach((box ) => {box.textContent = ""});
+//     const promises = [...container.children].map((_, i) => creationPromise(i));
+//     // console.log(promises);
+//     Promise.allSettled(promises)
+//     .then(items =>{
+//         items.forEach((item, i)=>{
+//             console.log(item);
+//             setTimeout(()=>{
+//                 if(item.status === "fulfilled"){
+//                     counter += 1;
+//                 }
+//                 container.children[i].textContent=item.value || item.reason;
+                   
+//                 if(container.children.length - 1 === i){
+//                     setTimeout(()=>{
+//                         if(counter === container.children.length || !counter){
+//                             alert("Winner!");
+//                         }
+//                         else{
+//                             alert("Lost money!");
+//                         }
+//                     }, 500);
+//                   }
+//             }, i * 1000)
+//             // console.log(items);
+//         })
+
+// });
+// }
+
+
+// function creationPromise(deley) {
+//     return new Promise((res, rej) => {
+//         setTimeout(() => {
+//             const random = Math.random();
+//             if(random > .5){
+//                 res("🤑");
+//             }
+//             else{
+//                 rej("😈");
+//             }
+//         }, 1000 * deley);
+     
+//     })
+// }
--- src/js/page_10.js
+++ src/js/page_10.js
@@ -2,10 +2,52 @@ console.log('10');
 
 const btn = document.querySelector(".js_btn");
 const cont = document.querySelector(".js_container09")
-console.log(btn);
-console.log(cont);
+// console.log(btn);
+// console.log(cont);
 
 btn.addEventListener('click', onClickFun);
 function onClickFun(){
-    console.log(btn);
+    let arr = [];
+    // console.log([...cont.children]);
+    [...cont.children].forEach((box)=>{ box.textContent = '' });
+    [...cont.children].forEach((box, idx)=>{
+        // console.log(box, idx);
+        creationProm(idx)
+        .then((smile)=>{
+            // console.log(smile);
+            box.textContent = smile;
+            arr.push('1');
+           
+        })
+        .catch((smile)=>{
+            // console.log(smile);
+            box.textContent = smile;
+        })
+        .finally(()=>{
+            setTimeout(()=>{
+                if(idx === cont.children.length - 1 ){
+                    // alert('This is winner!')
+                    console.log(cont.children.length);
+                }
+            }, 500);
+           
+        })
+    })
+
 }
+
+function creationProm(num){
+    return new Promise((resp, rejp)=>{
+        setTimeout(()=>{
+            let random = Math.random();
+            if(random > .7){
+                resp('😺');
+            }
+            else{
+                rejp('👩‍🦰');
+            }
+        }, 1000 * num);
+    })
+};
+  
+
