62dd4596ae2dfd48d4f78f4123b68e6e22152035
Seeker2025 <85400723+Seeker2025@users.noreply.github.com>
2025-04-23 06:46:53 +0000

    Deploying to gh-pages from @ Seeker2025/experiments08@ed0aca472c6bdbee2bd28b707d0abd2148a469da 🚀

---
+++ page11.js
@@ -0,0 +1,2 @@
+import"./assets/styles-alkzcIgW.js";console.log(11);function l(){setInterval(()=>{let e=Math.random();new Promise((o,n)=>{e<.6?o("c"):n("f")}).then(o=>console.log(o)).catch(o=>console.log(o))},500)}l();new Promise(()=>{});
+//# sourceMappingURL=page11.js.map
