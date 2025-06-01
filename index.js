aa768b6a205641c7b33ff0f1b5edbf2fc04ca490
Seeker2025 <85400723+Seeker2025@users.noreply.github.com>
2025-06-01 18:49:00 +0000

    Deploying to gh-pages from @ Seeker2025/experiments08@e629dc1c5491a5e843ab958ad1f70c335222f004 🚀

--- index.js
+++ index.js
@@ -1,2 +1,2 @@
-import"./assets/styles-alkzcIgW.js";console.log("Hello World");
+import"./assets/styles-D_R2uvVm.js";console.log("Hello World");
 //# sourceMappingURL=index.js.map
--- page11.js
+++ page11.js
@@ -1,2 +1,2 @@
-import"./assets/styles-alkzcIgW.js";console.log(11);let l=Math.random();console.log(l);const s=new Promise((e,o)=>{l<.3?e("p1 Hi!"):o("p1 No")}),n=new Promise((e,o)=>{l>.4||l<.7?e("p2 Hi!"):o("p2 No")}),i=new Promise((e,o)=>{l>.7?e("p3 Hi!"):o("p3 No")});let p=[s,n,i];Promise.allSettled(p).then(e=>console.log(e));
+import"./assets/styles-D_R2uvVm.js";console.log(11);let l=Math.random();console.log(l);const s=new Promise((e,o)=>{l<.3?e("p1 Hi!"):o("p1 No")}),n=new Promise((e,o)=>{l>.4||l<.7?e("p2 Hi!"):o("p2 No")}),i=new Promise((e,o)=>{l>.7?e("p3 Hi!"):o("p3 No")});let p=[s,n,i];Promise.allSettled(p).then(e=>console.log(e));
 //# sourceMappingURL=page11.js.map
